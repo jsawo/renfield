97ba89d3433358652972285855a1da8ab97c0ad0
Jacek Sawoszczuk <jacek.sawoszczuk@gmail.com>
2022-10-24 21:06:00 +0200

    Editor tweaks (#2)
    allow mouse wheel zooming
    control format on paste
    style editor splits

--- frontend/src/components/Editor.vue
+++ frontend/src/components/Editor.vue
@@ -1,27 +1,31 @@
 <script lang="ts" setup>
 import Editor from '@guolao/vue-monaco-editor'
 
-defineProps<{
+withDefaults(defineProps<{
   value: string,
   language: string,
-}>()
-
+  formatOnPaste?: boolean,
+}>(), {
+  formatOnPaste: true,
+})
 </script>
 
 <template>
   <Editor
-    :value="value"
-    theme='vs-dark'
-    :defaultLanguage="language"
-    :options="{
-      lineNumbers: 'off',
-      wordWrap: 'on',
-      minimap: {
-        enabled: false,
-      },
-      scrollBeyondLastLine: false,
-      renderWhitespace: 'none',
-      fontSize: 16,
-    }"
+      :value="value"
+      theme='vs-dark'
+      :defaultLanguage="language"
+      :options="{
+        lineNumbers: 'off',
+        wordWrap: 'on',
+        minimap: {
+          enabled: false,
+        },
+        scrollBeyondLastLine: false,
+        fontSize: 16,
+        padding: {top: 10, bottom: 10},
+        mouseWheelZoom: true,
+        formatOnPaste: formatOnPaste,
+      }"
   />
 </template>

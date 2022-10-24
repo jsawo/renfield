80a4cccfb168cbb976906b190b2339f2401e3ce6
Jacek Sawoszczuk <jacek.sawoszczuk@gmail.com>
2022-10-24 22:07:19 +0200

    Handle beam clipboard (#3)
    add buttons to copy sample beam strings to clipboard
    plus a button to clear beam messages

--- app.go
+++ app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/wailsapp/wails/v2/pkg/runtime"
+	"golang.design/x/clipboard"
 )
 
 type App struct {
@@ -11,10 +12,19 @@ type App struct {
 }
 
 func NewApp() *App {
+	err := clipboard.Init()
+	if err != nil {
+		panic(err)
+	}
+
 	return &App{}
 }
 
 func (a *App) OpenDirectoryDialog() string {
 	result, _ := runtime.OpenDirectoryDialog(a.ctx, runtime.OpenDialogOptions{})
 	return result
+}
+
+func (a *App) CopyToClipboard(data string) {
+	clipboard.Write(clipboard.FmtText, []byte(data))
 }
--- frontend/src/components/pages/Beam.vue
+++ frontend/src/components/pages/Beam.vue
@@ -1,19 +1,66 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
+import { CopyToClipboard } from '@wails/go/main/App'
 
 defineProps<{
   messages: Array<BeamMessage>,
 }>()
 
+const port = '3333'
+
+function copyLaravelString() {
+  const data = `Http::post("http://localhost:${port}/beam", ["payload"=> json_encode(["foo" => "bar"])]);`
+  CopyToClipboard(data)
+  showNotification.value = true
+}
+
+function copyCurlString() {
+  const data = `curl -X POST -d '{"payload": "{ \\\"foo\\\": \\\"bar\\\" }"}' http://localhost:${port}/beam`
+  CopyToClipboard(data)
+  showNotification.value = true
+}
+
+function getPayload(payload: string): string {
+  try {
+    return JSON.stringify(JSON.parse(payload), null, 2)
+  } catch (e) {
+    return payload
+  }
+}
+
+const showNotification = ref(false)
 </script>
 
 <template>
   <main>
-    <div>Beam (:3333)</div>
+    <w-notification v-model="showNotification" :timeout="1000" success plain round shadow absolute>
+      Copied to clipboard.
+    </w-notification>
+
+    <div>
+      Beam (running on port :{{ port }})
+      <w-button class="ml2" outline @click="copyLaravelString">
+        Laravel
+        <w-icon sm class="ml1" xl>mdi mdi-clipboard-multiple-outline</w-icon>
+      </w-button>
+      <w-button class="ml2" outline @click="copyCurlString">
+        CURL
+        <w-icon sm class="ml1" xl>mdi mdi-clipboard-multiple-outline</w-icon>
+      </w-button>
+
+      <div style="float: right;">
+        <w-button outline @click="$emit('clearBeamMessages')">
+          <w-icon sm class="mr1" xl>mdi mdi-delete-outline</w-icon>
+          Clear
+        </w-button>
+      </div>
+    </div>
+
     <ul class="log_box">
-      <li v-for="msg in messages">
+      <li v-for="(msg, index) in messages" :key="index">
         <w-card class="mt2">
           <div class="title5">Date here</div>
-          <pre class="payload">{{ JSON.stringify(JSON.parse(msg.Payload), null, 2) }}</pre>
+          <pre class="payload">{{ getPayload(msg.Payload) }}</pre>
         </w-card>
       </li>
     </ul>

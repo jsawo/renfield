7527e7751357b196621c81547532101c929a30c1
Jacek <jacek.sawoszczuk@gmail.com>
2023-03-29 21:01:52 +0200

    simple editor tabs

---
+++ frontend/src/components/EditorTab.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+defineProps<{
+  active: boolean,
+}>();
+</script>
+<template>
+  <div
+    :class="[active 
+      ? 'bg-stone-900 text-stone-200 border-stone-600' 
+      : 'text-stone-300 border-transparent', 
+      'px-3 py-2 cursor-pointer relative border-y-0 border-x border-transparent bg-stone-800 text-xs hover:border-stone-700'
+    ]"
+  >
+    <slot />
+  </div>
+</template>
---
+++ frontend/src/components/EditorTabBar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex bg-stone-800">
+    <slot />
+  </div>
+</template>
